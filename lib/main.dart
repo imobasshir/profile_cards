984d5416cb73d052479918a2f0aa9a8cb930d949
imobasshir <iibrahimimam24@gmail.com>
2021-12-13 23:51:12 +0530

    app bar added

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Profile Card'),
+        ),
         // backgroundColor: Colors.white,
         body: SafeArea(
           child: Column(
